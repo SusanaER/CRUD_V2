fa9003fea648e5b16cd93b58c84884f980108332
SusanaER <susana.esparza1213@gmail.com>
2022-03-16 21:18:00 -0600

    PassengerMicroservice

---
+++ PassengerMicroservice/PassengerMicroservice.ApplicationServices/Passengers/PassengerAppService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassengerMicroservice.Core.Passenger;
+using PassengerMicroservice.DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassengerMicroservice.ApplicationServices.Passengers
+{
+    public class PassengerAppService : IPassengerAppService
+    {
+        private readonly IRepository<int, Passenger> _repository;
+
+        public PassengerAppService(IRepository<int, Passenger> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> AddPassengerAsync(Passenger passenger)
+        {
+            await _repository.AddAsync(passenger);
+            return passenger.Id;
+        }
+
+        public async Task DeletePassengerAsync(int passengerId)
+        {
+            await _repository.DeleteAsync(passengerId);
+        }
+
+        public async Task EditPassengerAsync(Passenger passenger)
+        {
+            await _repository.UpdateAsync(passenger);
+        }
+
+        public async Task<Passenger> GetPassengerAsync(int passengerId)
+        {
+            return await _repository.GetAsync(passengerId);
+        }
+
+        public async Task<List<Passenger>> GetPassengersAsync()
+        {
+            var list = await _repository.GetAll().ToListAsync();
+            return list;
+        }
+    }
+}
---
+++ PassengerMicroservice/PassengerMicroservice.DataAccess/Repositories/PassengerRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassengerMicroservice.Core.Passenger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassengerMicroservice.DataAccess.Repositories
+{
+    public class PassengerRepository : Repository<int, Passenger>
+    {
+        public PassengerRepository(PassengerMicroserviceContext context) : base(context)
+        {
+
+        }
+
+        public override async Task<Passenger> AddAsync(Passenger entity)
+        {
+            await Context.Passengers.AddAsync(entity);
+
+            await Context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public override async Task<Passenger> UpdateAsync(Passenger entity)
+        {
+            var passenger = await Context.Passengers.FindAsync(entity.Id);
+            passenger.FirstName = entity.FirstName;
+            passenger.LastName = entity.LastName;
+            passenger.Age = entity.Age;
+            await Context.SaveChangesAsync();
+            return entity;
+        }
+
+        public override async Task<Passenger> GetAsync(int id)
+        {
+            var passenger = await Context.Passengers.FirstOrDefaultAsync(x => x.Id == id);
+            return passenger;
+        }
+    }
+}
